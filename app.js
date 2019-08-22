66de8f5c4875cb8fe1b56c73fd59c9081f70dd32
Tanuj Dave <tanuj.dave@printi.com.br>
2019-08-22 20:40:48 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,87 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
+const mongoose = require('mongoose');
+
+const Event = require('./models/event');
+
+const app = express();
+
+const events = []
+
+app.use(bodyParser.json());
+
+app.use('/graphql', graphqlHttp({
+    schema: buildSchema(`
+        type Event {
+            _id: ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
+        input EventInput {
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
+        type RootQuery {
+            events: [Event!]!
+        }
+
+        type RootMutation {
+            createEvent(eventInput: EventInput): Event
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+        events: () => {
+            return Event.find()
+                .then(events => {
+                    return events.map(event => {
+                        return {...event._doc};
+                    });
+                })
+                .catch(err => {
+                    throw err;
+                });
+        },
+        createEvent: args => {
+            const event = new Event({
+                title: args.eventInput.title,
+                description: args.eventInput.description,
+                price: +args.eventInput.price,
+                date: new Date(args.eventInput.date)
+            });
+
+            return event
+                .save()
+                .then(result => {
+                    return {...result._doc};
+                })
+                .catch(err => {
+                    throw err;
+                })
+        }
+    },
+    graphiql: true
+}));
+
+mongoose.connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-3s0ia.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`,
+    {
+        useNewUrlParser: true
+    }
+).then(() => {
+    app.listen(3000)
+}).then().catch(err => {
+    console.log(err)
+});
