07b079cc8fbc53869d067c9bd3d708a475383f85
Tanuj Dave <tanuj.dave@printi.com.br>
2019-08-29 16:49:13 +0530

    #10 Adding User Authentication | Build a Complete App with GraphQL, Node.js, MongoDB and React.js

--- graphql/resolvers/booking.js
+++ graphql/resolvers/booking.js
@@ -13,16 +13,22 @@ module.exports = {
             throw err;
         }
     },
-    bookEvent: async args => {
+    bookEvent: async (args, req) => {
+        if (!req.isAuth) {
+            throw new Error('Unauthenticated!');
+        }
         const fetchedEvent = await Event.findOne({ _id: args.eventId});
         const booking = new Booking({
-            user: '5d65146db39c03670ef93d39',
+            user: req.userId,
             event: fetchedEvent
         });
         const result = await booking.save();
         return transformBooking(result);
     },
-    cancelBooking: async args => {
+    cancelBooking: async (args, req) => {
+        if (!req.isAuth) {
+            throw new Error('Unauthenticated!');
+        }
         try {
             const booking = await Booking.findById(args.bookingId).populate('event');
             const event = transformEvent(booking.event);
--- graphql/resolvers/events.js
+++ graphql/resolvers/events.js
@@ -1,4 +1,5 @@
 const Event = require("../../models/event");
+const User = require("../../models/user");
 const { transformEvent } = require('./merge');
 
 module.exports = {
@@ -12,20 +13,23 @@ module.exports = {
             throw err;
         }
     },
-    createEvent: async args => {
+    createEvent: async (args, req) => {
+        if (!req.isAuth) {
+            throw new Error('Unauthenticated!');
+        }
         const event = new Event({
             title: args.eventInput.title,
             description: args.eventInput.description,
             price: +args.eventInput.price,
             date: new Date(args.eventInput.date),
-            creator: "5d65146db39c03670ef93d39"
+            creator: req.userId
         });
         let createdEvent;
 
         try {
             const result = await event.save();
             createdEvent = transformEvent(event);
-            const creator = await User.findById("5d65146db39c03670ef93d39");
+            const creator = await User.findById(req.userId);
             if (!creator) {
                 throw new Error("User not exist");
             }
